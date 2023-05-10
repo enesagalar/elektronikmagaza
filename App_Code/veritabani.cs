f6a7948776574b602c9eba21ce0df0d9457d3ab3
Enes <enesagalar@gmail.com>
2023-05-10 22:42:02 +0300

    elektronikmagaza
    Elektronik Mağaza adlı C# tabanlı bir website çalışması

---
+++ App_Code/veritabani.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// veritabani için özet açıklama
+/// </summary>
+public class veritabani
+{
+    string strveritabanibaglantisi = ConfigurationManager.ConnectionStrings["elektromagazadb"].ToString();
+
+
+    public string anamenu()
+    {
+
+        DataTable dt = DataTableGetir("select * from Anamenu order by Siralama");
+        string strdata = "";
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            strdata += "<li class=\"nav-item mr-lg-2 mb-lg-0 mb-2\"><a class=\"nav-link\" href=\"" + dt.Rows[i]["AcacagiSayfa"] + "\" target=\"" + dt.Rows[i]["AcilisSekli"] + "\">" + dt.Rows[i]["MenuAdi"] + "</a></li>";
+        }
+        return strdata;
+
+
+    }
+
+    public string sosyalsitebaglantilari()
+    {
+        DataTable dt = DataTableGetir("select * from SosyalSiteBaglantisi where Durum='Açık' order by Siralama");
+        string strdata = "";
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            //strdata += "<li><a class= \"icon tw \" href= \"" + dt.Rows[i]["AcacagiSayfa"] + "\"target=\"blank\"><i class= \"fa-" + dt.Rows[i]["SiteAdi"] + " \"></i></a></li>";
+            strdata += "<li><a href=\"" + dt.Rows[i]["AcacagiSayfa"] + "\" target=\"_blank\"><i class=\"fab fa-" + dt.Rows[i]["SiteAdi"] + "\"></i></a></li>";
+        }
+        //strdata += "<li></li>";
+        return strdata;
+
+    }
+    public void ekle_sil_guncelle(string sorgu)
+    {
+        using (SqlConnection baglanti = new SqlConnection(strveritabanibaglantisi))
+        {
+            using (SqlCommand cmd = new SqlCommand(sorgu, baglanti))
+            {
+                cmd.CommandType = CommandType.Text;
+                baglanti.Open();
+                cmd.ExecuteNonQuery();
+                baglanti.Close();
+            }
+        }
+    }
+    public DataTable DataTableGetir(string sorgu)
+    {
+        DataTable dt = new DataTable();
+        using (SqlConnection baglanti = new SqlConnection(strveritabanibaglantisi))
+        {
+            using (SqlCommand cmd = new SqlCommand(sorgu, baglanti))
+            {
+                baglanti.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sorgu, baglanti);
+                try
+                {
+                    adapter.Fill(dt);
+                }
+                catch
+                {
+
+                }
+                baglanti.Close();
+            }
+        }
+        return dt;
+    }
+
+    //bu fonksiyon kendisine gönderilen sorgu sonucu dönen kayıtları radiobuttonlist e doldurur.
+    public void rbtnllist_aktar(string sql, System.Web.UI.WebControls.RadioButtonList rbtnl)
+    {
+        rbtnl.Items.Clear();
+        using (SqlConnection baglanti = new SqlConnection(strveritabanibaglantisi))
+        {
+            using (SqlCommand cmd = new SqlCommand(sql, baglanti))
+            {
+                baglanti.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        rbtnl.Items.Add(reader[0].ToString());
+                    }
+                }
+                finally
+                {
+                    if (reader != null)
+                        ((IDisposable)reader).Dispose();
+                }
+                baglanti.Close();
+            }
+
+        }
+    }
+    public string verigetir(string sorgu, string alan)
+    {
+        string strdata = "";
+
+        using (SqlConnection baglanti = new SqlConnection(strveritabanibaglantisi))
+        {
+            using (SqlCommand cmd = new SqlCommand(sorgu, baglanti))
+            {
+                baglanti.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        strdata = reader["" + alan + ""].ToString();
+                    }
+
+                }
+                finally
+                {
+                    if (reader != null)
+                        ((IDisposable)reader).Dispose();
+                }
+
+                baglanti.Close();
+
+            }
+
+        }
+        return strdata;
+
+
+    }
+}
---
+++ index.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class web_index : System.Web.UI.Page
+{
+    veritabani vtislemler = new veritabani();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+        anamenu.InnerHtml = vtislemler.anamenu();
+
+        sosyalsitebaglantilarialt.InnerHtml = vtislemler.sosyalsitebaglantilari();
+
+        //Anketler tablosundaki Durum='Aktif' olan anketin AnketID verisini buldum.
+        string aktifanketid = vtislemler.verigetir("select AnketID from Anketler where Durum='Aktif'", "AnketID");
+        //Üst satırda bulduğum AnketID ye sahip soruyu AnketSoruları tablosundan buldum ve id, runat="server" özelliklerini verdiğim etikete gönderdim.
+        anketsorusu.InnerHtml = vtislemler.verigetir("select Soru from AnketSorulari where AnketID='" + aktifanketid + "'", "Soru");
+        //cevaplar için eklediğim rbtnlanketcevaplari id sine sahip radiobuttonlist içine bulduğum AnketID ye sahip cevapları doldurdum.
+        vtislemler.rbtnllist_aktar("select Cevap from AnketCevaplari where AnketID='" + aktifanketid + "' order by Siralama", rbtnlanketcevaplari);
+        rbtnlanketcevaplari.ClearSelection();
+        }
+    }
+
+    protected void btncevapgonder_Click(object sender, EventArgs e)
+    {
+        //Anketler tablosundaki Durum='Aktif' olan anketin AnketID verisini buldum.
+        string aktifanketid = vtislemler.verigetir("select AnketID from Anketler where Durum='Aktif'", "AnketID");
+        //seçilen cevaba ait CevapID verisini buldum.
+        string secilencevapid = vtislemler.verigetir("select CevapID from AnketCevaplari where AnketID='" + aktifanketid + "' and Cevap='" + rbtnlanketcevaplari.SelectedItem.Text + "'", "CevapID");
+        //bulduğum CevapID ye sahip cevabın CevapAdet alanındaki değerini 1 artırarak update ettim.
+        vtislemler.ekle_sil_guncelle("update AnketCevaplari set CevapAdet=CevapAdet+1 where CevapID='" + secilencevapid + "'");
+        rbtnlanketcevaplari.ClearSelection();
+        lblislemtamam.Visible = true;
+        
+        if (Timer1.Enabled == true)
+        {
+            Timer1.Enabled = false;
+        }
+        else
+        {
+            Timer1.Enabled = true;
+        }
+    }
+
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        Timer1.Enabled = false;
+        lblislemtamam.Visible = false;
+    }
+}
